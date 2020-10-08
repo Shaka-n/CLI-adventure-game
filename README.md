b146ccad3df7fdaf54aee1086e1b7645c8201ea7
Garen Sahagian <g.s.sahagian@gmail.com>
2020-10-08 09:13:41 -0400

    I fixed the rakefile. It contained a rogue nil for some reason. Rake runs properly now. I also updated the ReadMe to reflect the new project goals.

--- README.md
+++ README.md
@@ -1,13 +1,7 @@
-Escaping Quarantine
+The purpose of this little application is to provide a simple and fun way to play around with some of the more arcane Active Record Associations in the context of a text adventure game. My goal is to find a way to integrate all of the most interesting active record associations (or at least the ones I personally find the most confusing) in some way that enhances the player experience. Should this project progress far enough, I may publish it as a gem. I think it would be fun to create a method for rapidly spinning up a text-adventure game from a console. 
+
+Development Game: Escaping Quarantine
 
 This is a short terminal-based adventure about getting outside during a quarantine. The player navigates the game world by inputting commands through the number keys and by typing responses to prompts (i.e. 'yes' or 'no'). The core gameplay loop is to walk into a room and to search the furniture until the player finds both a 'mask' item and a 'gloves' item. Once the player has found both items, they can navigate to the front door and walk outside. At that point, their score is determined by the number of moves they needed to complete the game. Their score is saved and added to a permanent highscore roster, and the game ends.
 
-Installation
 
-Download the game files, from the main game directory run rake db:migrate in the terminal. Run the game with "ruby bin/game.rb".
-
-Contributors
-
-If you'd like contribute to this project, we have a few suggestions on where to start. The Polymorphic branch currently suffers from a bug that instantiates objects that are not stored in the database, but also are not stored in variables. It's probably a simple fix, but we don't have time. Fixing that would allow for more flexible inventory functionality, which would significantly increase gameplay options.
-
-License
--- db/schema.rb
+++ db/schema.rb
@@ -2,18 +2,18 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200417055007) do
+ActiveRecord::Schema.define(version: 2020_04_17_055007) do
 
   create_table "furnishings", force: :cascade do |t|
-    t.string  "name"
+    t.string "name"
     t.integer "room_id"
   end
 
@@ -22,27 +22,27 @@ ActiveRecord::Schema.define(version: 20200417055007) do
   end
 
   create_table "items", force: :cascade do |t|
-    t.string  "name"
+    t.string "name"
     t.integer "grabbable_id"
-    t.string  "grabbable_type"
+    t.string "grabbable_type"
     t.index ["grabbable_type", "grabbable_id"], name: "index_items_on_grabbable_type_and_grabbable_id"
   end
 
   create_table "players", force: :cascade do |t|
-    t.string  "name"
+    t.string "name"
     t.integer "room_id"
     t.boolean "gloves"
     t.boolean "mask"
   end
 
   create_table "rooms", force: :cascade do |t|
-    t.string  "name"
+    t.string "name"
     t.integer "proximal_room_id"
     t.index ["proximal_room_id"], name: "index_rooms_on_proximal_room_id"
   end
 
   create_table "scores", force: :cascade do |t|
-    t.string  "name"
+    t.string "name"
     t.integer "score"
   end
 
